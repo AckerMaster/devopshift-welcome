7a75499d2562999b1140622a1088c6c175bddce0
Your Name <you@example.com>
2025-02-04 12:26:17 +0000

    added content to main.tf file

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -5,3 +5,35 @@ provider "aws" {
 variable "region" {
   default = "us-east-1"
 }
+
+resource "aws_security_group" "sg" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "vm" {
+  ami           = "ami-0ff8a91507f77f867"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "liad-vm"
+  }
+}
+
+output "vm_public_ip" {
+  value       = aws_instance.vm.public_ip
+  description = "Public IP address of the VM"
+}
