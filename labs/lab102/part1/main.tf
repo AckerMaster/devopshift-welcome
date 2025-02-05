a3a6e19f678e86877f3907a855d7163ff7bab2a4
Your Name <you@example.com>
2025-02-05 12:01:48 +0000

    added data structure

--- labs/lab102/part1/main.tf
+++ labs/lab102/part1/main.tf
@@ -1,16 +1,24 @@
-variable "emptyip" {
-  default = ""
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
 }
 
 
-resource "null_resource" "check_public_ip" {
-  provisioner "local-exec" {
-    command = <<EOT
-      if [ -z "${var.emptyip}" ]; then
-        echo "ERROR: Public IP address was not assigned." >&2
-        exit 1
-      fi
-    EOT
+data "aws_instances" "yaniv_vm" {
+  filter {
+    name   = "tag:Name"
+    values = ["yaniv-vm"]
   }
 }
 
+data "aws_instance" "yaniv_vm_details" {
+  instance_id = tolist(data.aws_instances.yaniv_vm.ids)[0]
+}
+
+output "lecturer_public_ip" {
+  value       = data.aws_instance.yaniv_vm_details.public_ip
+  description = "Public IP address of the lecturer's VM"
+}
