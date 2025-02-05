68686160c443395422ddf0965efcce9feb2e53a2
Your Name <you@example.com>
2025-02-05 09:50:27 +0000

    added muck part for lab 102 to check if check_public_ip works

---
+++ labs/lab102/part1/main.tf
@@ -0,0 +1,16 @@
+variable "emptyip" {
+  default = ""
+}
+
+
+resource "null_resource" "check_public_ip" {
+  provisioner "local-exec" {
+    command = <<EOT
+      if [ -z "${var.emptyip}" ]; then
+        echo "ERROR: Public IP address was not assigned." >&2
+        exit 1
+      fi
+    EOT
+  }
+}
+
