00f027611a042b05d5f9084b4f739abd6045bae0
Your Name <you@example.com>
2025-02-06 09:22:33 +0000

    added ec2 module

---
+++ labs/lab500/labmodule/modules/ec2/main.tf
@@ -0,0 +1,83 @@
+provider "aws" {
+ region = var.region
+}
+
+variable "region" {
+ default = "us-east-1"
+}
+
+variable "machine_type" {}
+
+variable "ami" {}
+
+variable "machine_name" {}
+
+# Mocked IP var
+variable "empty_ip" {
+   default = ""
+}
+
+
+resource "aws_security_group" "sg" {
+ ingress {
+   from_port   = 22
+   to_port     = 22
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+resource "aws_instance" "vm" {
+  ami           = var.ami
+ instance_type = var.machine_type
+
+
+ vpc_security_group_ids = [aws_security_group.sg.id]
+
+
+ tags = {
+   Name = var.machine_name
+ }
+}
+
+output "vm_public_ip" {
+ value       = aws_instance.vm.public_ip
+ description = "Public IP address of the VM"
+ depends_on = [ null_resource.check_public_ip ]
+}
+
+resource "null_resource" "check_public_ip" {
+ provisioner "local-exec" {
+   command = <<EOT
+     if [ -z "${aws_instance.vm.public_ip}" ]; then
+       echo "ERROR: Public IP address was not assigned." >&2
+       exit 1
+       else
+       echo "We got the IP! ${aws_instance.vm.public_ip}"
+     fi
+   EOT
+ }
+
+ depends_on = [aws_instance.vm]
+}
+
+output "print_machine_name" {
+    value = var.ami
+}
+
+output "print_machine_type" {
+    value = var.machine_type
+}
+
+output "print_ami" {
+    value = var.ami
+}
--- labs/lab500/labmodule/output.tf
+++ labs/lab500/labmodule/output.tf
@@ -1,13 +1,22 @@
-module "outputdemo" {
-  
-  source = "./modules/outputdemo" 
-  instance_type = "t2.micro"
-  subnrt_id = "10.0.0.1/16"
-  ami = "i0-3431512441fg"
-  message = "hello class"
-
+module "ec2" {
+  source       = "./modules/ec2"
+  ami          = "ami-0c02fb55956c7d316"
+  machine_type  = "t2.micro"
+  machine_name  = "liad-vm"
 }
 
-output "test" {
-  value = module.outputdemo
+output "print_machine_name" {
+  value = module.ec2.print_machine_name
+}
+
+output "print_machine_type" {
+  value = module.ec2.print_machine_type
+}
+
+output "print_ami" {
+  value = module.ec2.print_ami
+}
+
+output "vm_public_ip" {
+  value = module.ec2.vm_public_ip
 }
