bb340fc795e3a2410c3237066fc27b235fb46a97
Your Name <you@example.com>
2025-02-21 11:51:03 +0000

    added the enabling public ip to the template

--- python-test/script.py
+++ python-test/script.py
@@ -40,6 +40,7 @@ resource "aws_instance" "web_server" {
   instance_type     = "{{ instance_type }}"
   availability_zone = "{{ availability_zone }}"
   subnet_id        = aws_subnet.public[0].id
+  associate_public_ip_address = true
 
   tags = {
     Name = "WebServer"
