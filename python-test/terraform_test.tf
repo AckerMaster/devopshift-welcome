ef53762850eef32bfb1c090996c44dcb051fc8cf
Your Name <you@example.com>
2025-02-21 09:40:08 +0000

    associated the sg with the alb

--- python-test/terraform_test.tf
+++ python-test/terraform_test.tf
@@ -32,11 +32,20 @@ resource "aws_security_group" "lb_sg" {
   name        = "lb_security_group_${random_string.suffix.result}"
   description = "Allow HTTP inbound traffic"
 
+  vpc_id =aws_vpc.main.id
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outgoing traffic
   }
 }
 
