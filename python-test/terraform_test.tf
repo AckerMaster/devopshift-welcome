ff94eb8193cf111c4edc10c4f4d68ea4f7375a8c
Your Name <you@example.com>
2025-02-21 09:27:28 +0000

    created igw, attached it to the vpc and associated with route table

--- python-test/terraform_test.tf
+++ python-test/terraform_test.tf
@@ -75,6 +75,31 @@ resource "aws_vpc" "main" {
 }
 
 
+resource "aws_internet_gateway" "main_gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MainInternetGateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_gw.id
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  count           = 2
+  subnet_id       = aws_subnet.public[count.index].id
+  route_table_id  = aws_route_table.public_rt.id
+}
+
+
+
 output "instance_id" {
   value = aws_instance.web_server.id
 }
